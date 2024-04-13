f37170cc4ce75a77b5b0f4ede9d108a10f538177
Mahendranath K <mahendranathkundla13@gmail.com>
2024-04-13 10:09:30 +0530

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,31 @@
+const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const cypressSplit = require('cypress-split')
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // require('cypress-mochawesome-reporter/plugin')(on);
+      // on('file:preprocessor', webpackPreprocessor);
+      // allureWriter(on, config);
+      // return config;
+      allureWriter(on, config);
+      return config;
+    },
+    viewportHeight:1000,
+    viewportWidth:600,
+    defaultCommandTimeout:8000,
+    pageLoadTimeout:8000,
+    includeShadowDom:true,
+    chromeWebSecurity:false,
+    baseUrl:'https://magento.softwaretestingboard.com/',
+    failOnStatusCode: false,
+    "retries": {      
+      "runMode": 1,      
+      "openMode": 1
+    }
+   
+    
+  },
+});
